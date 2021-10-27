f0acb995093277d1355bc1451bf1d1176194abe4
tomatoaas <tomomi.314159265@gmail.com>
2021-10-27 11:52:23 +0900

    [add]自動スクロール

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Popup = () => {
+
+  const scroll = () =>{
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: `scroller = setInterval(function(){scrollTo(scrollX, scrollY+1)}, 40);`})
+    })
+  }
+
   return (
     <div
       style={{ backgroundColor: 'blue', color: 'white', textAlign: 'center' }}
+      onClick={scroll}
     >
       This is popup
     </div>
