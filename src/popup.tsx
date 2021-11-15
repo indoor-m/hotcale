9a57172440eb7401318fca0845762688f146591a
tomatoaas <tomomi.314159265@gmail.com>
2021-11-15 16:43:38 +0900

    [update]tailwindの設定を追加

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../static/style.css'
 
 const Popup = () => {
 
@@ -14,10 +15,41 @@ const Popup = () => {
 
   return (
     <div
-      className={'bg-blue-500'}
+      className={'w-60 py-3 px-7'}
       onClick={scroll}
     >
-      This is popup
+      <div
+        className={'py-1'}
+      >
+        スクロールオプション
+      </div>
+      <div
+        className={'py-1 flex justify-between items-centor'}
+        
+      >
+        <div>
+          自動スクロール有効化
+        </div>
+        <div className="relative inline-block w-9 mr-2 align-middle select-none transition duration-200 ease-in ">
+          <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+          <label htmlFor="toggle" className="toggle-label block overflow-hidden h-5 rounded-full bg-gray-300 cursor-pointer"></label>
+        </div>
+      </div>
+
+      <div
+        className={'py-1'}
+      >
+        <div>
+          戻るときにリロードを行う
+        </div>
+        
+      </div>
+      <div
+        className={'py-1'}
+      >
+        スクロールの速さ
+      </div>
+      
     </div>
   )
 }
