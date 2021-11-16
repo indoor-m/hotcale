81d91fb2ac35bf3ee89ef8201aec829771cdb16c
tomatoaas <tomomi.314159265@gmail.com>
2021-11-16 16:32:48 +0900

    [add]popup画面にtoggleボタンを追加し、scrollのオンオフをできるようにした

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState } from "react"
 import ReactDOM from 'react-dom'
 import '../static/style.css'
 
 const Popup = () => {
 
-  const scroll = () =>{
-    //タブを取得
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      //表示中のタブでスクロールを実行
-      chrome.tabs.executeScript(tabs[0].id, {
-        code: `scroller = setInterval(function(){scrollTo(scrollX, scrollY+1)}, 40);`})
-    })
-  }
+  const [on, setOpen] = useState(false);
+
+  //スクロールの開始と停止
+  const handleClick = () => {
+    setOpen(!on);
+    if(!on){
+      /*
+      // スクロール機能
+      */
+      //タブを取得
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        //表示中のタブでスクロールを実行
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: `scroller = setInterval(function(){scrollTo(scrollX, scrollY+1)}, 20);`})
+      })
+    }else{
+      /*
+      // スクロール機能
+      */
+      //タブを取得
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        //表示中のタブでスクロールを実行
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: `clearInterval(scroller)`})
+      })
+    }
+    
+  };
 
   return (
     <div
       className={'w-60 py-3 px-7'}
-      onClick={scroll}
     >
       <div
         className={'py-1'}
@@ -31,8 +50,8 @@ const Popup = () => {
           自動スクロール有効化
         </div>
         <div className="relative inline-block w-9 mr-2 align-middle select-none transition duration-200 ease-in ">
-          <input type="checkbox" name="toggle" id="toggle" className="toggle-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
-          <label htmlFor="toggle" className="toggle-label block overflow-hidden h-5 rounded-full bg-gray-300 cursor-pointer"></label>
+          <input type="checkbox" onClick={handleClick} name="toggle" id="toggle" className="toggle-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+          <label htmlFor="toggle" className={`${on ? "is-scrollOn" : ""} toggle-label block overflow-hidden h-5 rounded-full bg-gray-300 cursor-pointer`}></label>
         </div>
       </div>
 
