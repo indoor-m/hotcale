87729141aaf08e5ec9c6b90b8f3ca3fe8dd6b66c
tingtt <ting.taku@gmail.com>
2021-11-17 10:28:42 +0900

    [add] 操作検知, 自動再開, 最上部へ戻る処理

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,9 +4,66 @@ import ReactDOM from 'react-dom'
 const Popup = () => {
   // スクロール処理
   const scroll = () => {
-    // 1pxスクロールする処理
-    const scroll_1 = () => {
-      scrollTo(scrollX, scrollY + 1)
+    // 実行する処理
+    const main = () => {
+      // スクロール処理を走らせるオブジェクト
+      let scrollerIntervalObject: NodeJS.Timer = null
+
+      // スクロール速度
+      const scrollInterval = 40
+
+      // 再開までの時間
+      const resumeInterval = 5000
+
+      // scrollY保持用フィールド
+      let observedScrollY: number
+
+      // スクロール中断
+      const pauseScroll = () => {
+        // スクロール停止
+        clearInterval(scrollerIntervalObject)
+        console.log('AutoScroll stopped.')
+
+        // 再開処理を予約
+        setTimeout(startScroll, resumeInterval)
+      }
+
+      // Y座標を監視しながらスクロール
+      const scroll = () => {
+        // 操作検知
+        if (observedScrollY != undefined && observedScrollY + 1 != scrollY) {
+          pauseScroll()
+        }
+
+        // 最下部検知
+        if (
+          document.documentElement.scrollHeight ==
+          scrollY + document.documentElement.clientHeight
+        ) {
+          // 最上部に戻る
+          scrollTo(scrollX, 0)
+        }
+
+        // scrollYを保持
+        observedScrollY = scrollY
+
+        // 1pxスクロール
+        scrollTo(scrollX, scrollY + 1)
+      }
+
+      // スクロール開始
+      const startScroll = () => {
+        // scrollYを初期化
+        observedScrollY = undefined
+
+        // スクロール開始
+        scrollerIntervalObject = setInterval(scroll, scrollInterval)
+
+        console.log('AutoScroll started.')
+      }
+
+      // スクロール開始
+      startScroll()
     }
 
     //タブを取得
@@ -14,7 +71,7 @@ const Popup = () => {
       //表示中のタブでスクロールを実行
       chrome.tabs.executeScript(tabs[0].id, {
         // 1pxスクロールをインターバル指定で実行
-        code: `scroller = setInterval(${scroll_1.toString()}, 40);`,
+        code: `(${main.toString()})()`,
       })
     })
   }
