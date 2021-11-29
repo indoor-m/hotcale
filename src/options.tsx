44692a8beb33c136ddcedaeac26c0cd091edf223
K-shir0 <kakishiro2190394@gmail.com>
2021-11-29 14:53:52 +0900

    update: ページ切り替え機能を追加

--- src/options.tsx
+++ src/options.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 const Options = () => {
   return (
-    <div
-      style={{ backgroundColor: 'blue', color: 'white', textAlign: 'center' }}
-    >
-      This is options
-    </div>
+    // インデックスが /options.html なので basename が必要
+    <BrowserRouter basename={'/options.html'}>
+      <Routes>
+        <Route index element={<Index />} />
+        <Route path={'/a'} element={<PageA />} />
+        <Route path={'b'} element={<PageB />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+const Index = () => (
+  <>
+    <div>Index Page</div>
+    <Link to={'/a'}>A Page</Link>
+    <Link to={'/b'}>B Page</Link>
+  </>
+)
+
+const PageA = () => (
+  <>
+    <div>A Page</div>
+    <Link to={'/'}>戻る</Link>
+    <Link to={'/b'}>B Page</Link>
+  </>
+)
+
+const PageB = () => (
+  <>
+    <div>B Page</div>
+    <Link to={'/'}>戻る</Link>
+    <Link to={'/a'}>A Page</Link>
+  </>
+)
 
 const container = document.getElementById('container')
 ReactDOM.render(<Options />, container)
