db128d4de3460b89354bda9145bad81ca66d4985
tingtt <ting.taku@gmail.com>
2021-12-15 11:48:33 +0900

    [wip] [add] 巡回スクロール(巡回リンク決め打ち)

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,12 @@
-import { startTabScroll } from './utils/scrollControl'
+import { startTabScroll, setTabNextUrl } from './utils/scrollControl'
+
+const urlStack = [
+  'https://github.com/indoor-m/hotcale/pull/1',
+  'https://github.com/indoor-m/hotcale/pull/2',
+  'https://github.com/indoor-m/hotcale/pull/3',
+  'https://github.com/indoor-m/hotcale/pull/4',
+  'https://github.com/indoor-m/hotcale/pull/5',
+]
 
 // ロード終了時の処理
 chrome.tabs.onUpdated.addListener(function (tabId, info) {
@@ -16,7 +24,15 @@ chrome.tabs.onUpdated.addListener(function (tabId, info) {
       // 開いているタブを検索
       chrome.tabs.query({ active: true }, (tabs) => {
         // スクロール中のタブidと一致
-        if (tabs.find((tab) => tab.id == object.currentTabId)) {
+        const tab = tabs.find((tab) => tab.id == object.currentTabId)
+        if (tab) {
+          if (urlStack[0] == tab.url) {
+            urlStack.push(urlStack.shift())
+            console.log('Rotated')
+
+            setTabNextUrl(tabId, urlStack[0])
+          }
+
           // スクロール開始
           console.log(`スクロール開始 tabId: ${tabId}`)
           startTabScroll(tabId)
