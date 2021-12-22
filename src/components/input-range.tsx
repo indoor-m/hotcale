122b51bc23410dd066042fddff6240f098c2f6f7
tomatoaas <tomomi.314159265@gmail.com>
2021-12-22 10:05:52 +0900

    [fix]valueを消した。

--- src/components/input-range.tsx
+++ src/components/input-range.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
 
 export const InputRange = () => (
-  <input
-    type="range"
-    min="0"
-    max="100"
-    step="10"
-    value="20"
-    className={'input-range'}
-  />
+  <input type="range" min="0" max="100" step="10" className={'input-range'} />
 )
