5bc3c1715453d1162bde621903a777050d5e8b41
tomatoaas <tomomi.314159265@gmail.com>
2021-12-22 09:50:21 +0900

    [add]input-rangeのstepと初期値を追加

--- src/components/input-range.tsx
+++ src/components/input-range.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export const InputRange = () => (
-  <input type="range" min="1" max="100" step="1" className={'input-range'} />
+  <input
+    type="range"
+    min="0"
+    max="100"
+    step="10"
+    value="20"
+    className={'input-range'}
+  />
 )
