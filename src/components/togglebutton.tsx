6aa8ea3f175b85d6a97ae0ff309a9c5be7fb430a
tomatoaas <tomomi.314159265@gmail.com>
2022-01-24 17:52:12 +0900

    [fix]toggleのコンポーネントを修正

--- src/components/togglebutton.tsx
+++ src/components/togglebutton.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-export const ToggleButton = () => (
-  <div className="relative inline-block w-9 align-middle select-none transition duration-200 ease-in">
+interface Props {
+  id?: string
+}
+
+export const ToggleButton: React.FC<Props> = ({ id }) => (
+  <div className="ml-2 relative inline-block w-9 align-middle select-none transition duration-200 ease-in">
     <input
       type="checkbox"
-      name="scroll"
-      id="scroll"
-      className="scroll-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"
+      id={id}
+      className="auto-scroll-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"
     />
     <label
-      htmlFor="scroll"
-      className={`scroll-label block overflow-hidden h-5 rounded-full bg-gray-300 cursor-pointer`}
+      htmlFor={id}
+      className={`auto-scroll-label block overflow-hidden h-5 rounded-full bg-gray-300 cursor-pointer`}
     />
   </div>
 )
