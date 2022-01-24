592e2b5b6165aeb167b9a6fe1ae6c5f9a1bae183
K-shir0 <kakishiro2190394@gmail.com>
2022-01-24 19:13:45 +0900

    fix:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: {
-    content: ['./src/*/*.tsx'],
+    content: ['./src/*.tsx', './src/*/*.tsx'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
