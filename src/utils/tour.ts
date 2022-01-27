daf9da809210f67eb05fbe46102ecd4613039c29
K-shir0 <kakishiro2190394@gmail.com>
2022-01-27 16:53:18 +0900

    fix:

--- src/utils/tour.ts
+++ src/utils/tour.ts
@@ -2,7 +2,11 @@ import { Log, Tour } from '../atoms/interfaces/tour'
 import { chromeStorageActions } from './base/chromeStorage'
 
 // ログを追加するための処理
-export const addLog = (tourId: string, log: Log, callback?: () => void) => {
+export const addLog = (
+  tourId: string,
+  log: Log,
+  callback?: () => void
+): void => {
   chromeStorageActions.findById<Tour>('tours', tourId, (tour) => {
     const newTour: Tour = { ...tour, logs: [...tour.logs, log] }
 
